40c007f8af6f3aacb0fe8c75c370f727c6a9ac2d
Torsak R <kasrot@users.noreply.github.com>
2017-04-12 15:32:27 -0400

    Initial commit

---
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+    /// <summary>
+    /// This class provides various test scenerios to 
+    /// test the computation of FizzBuzz
+    /// </summary>
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        //To test the Fizz 
+        [TestMethod]
+        public void TestFizz()
+        {
+            int[] testData =new int[] {3,6,9,12,18,21,24,27,33};
+            foreach(int i in testData)
+            {
+                Assert.AreEqual("Fizz",Program.FizzBuzz(i));
+            }
+        }
+        //To test the Buzz
+        [TestMethod]
+        public void TestBuzz()
+        {
+            int[] testData = new int[] {-5,5,10,20,25,35,40 };
+            foreach (int i in testData)
+            {
+                Assert.AreEqual("Buzz", Program.FizzBuzz(i));
+            }
+        }
+        //To test the FizzBuzz
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            int[] testData = new int[] {0,15,30,45,60,75,90};
+            foreach (int i in testData)
+            {
+                Assert.AreEqual("FizzBuzz", Program.FizzBuzz(i));
+            }
+        }
+        //To test the non FizzBuzz values such as 1,4, etc. 
+        [TestMethod]
+        public void TestNonFizzValue()
+        {
+            int[] testData = new int[] {1,2,4,7,11,13,16};
+            foreach (int i in testData)
+            {
+                Assert.AreEqual(i.ToString(), Program.FizzBuzz(i));
+            }
+        }
+
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace FizzBuzz
+{/// <summary>
+/// This class computes the FizzBuzz from an user input
+/// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            //Getting a user input from the console
+            string userInputInString = Console.ReadLine();
+            int userValue;
+            if (int.TryParse(userInputInString, out userValue))
+            {
+                Console.WriteLine(FizzBuzz(userValue));
+            }
+            else
+            {
+                Console.WriteLine("User Input must be an integer");
+            }
+        }
+        /// <summary>
+        /// This method calculates whether  the input 
+        /// value is Fizz, Buzz, Both ,or none of these
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns>String Output </returns>
+        public static string FizzBuzz(int userInput)
+        {
+            //I chose this approach than the traditional - 
+            //if else because in this application user is entering number from the console
+            //If we were to build an app that processes a large amouunt of random numbers, you could 
+            //either use bitwise modulo or even traditional approach would be better  
+
+           string outPut = "" ;
+            if ((userInput % 3) == 0)
+            {
+                outPut+="Fizz";
+            }
+            if (userInput % 5 == 0)
+            {
+                outPut += "Buzz";
+            }
+            if (outPut.Length == 0)
+            {
+                outPut =userInput.ToString();
+            }
+            return outPut;
+
+        }
+    }
+}
